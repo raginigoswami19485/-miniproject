c520a7b2b38e6b6ec21af2ce8711faea349bf847
raginigoswami19485 <raginigoswami19485@gmail.com>
2025-01-15 01:29:29 +0530

    first commit

---
+++ agecalculator/string.js
@@ -0,0 +1,14 @@
+
+
+let agee=()=>{
+
+    let age=document.querySelector("#aga").value   // link input tag
+    let tim= new Date();
+    let result= tim.getFullYear()-age;
+    let show=document.querySelector("#show")   // link p tag
+    show.innerHTML=result
+
+
+ }
+
+ 
